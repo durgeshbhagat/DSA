666fba8884ee01df5fcf526643b828a424ddfb7e
Durgesh Kumar <durgeshit@gmail.com>
2023-03-11 16:28:16 +0530

    Added underflow, overflow condition, and other dtack fucntions

--- Module_2/Stack/stack_01.c
+++ Module_2/Stack/stack_01.c
@@ -4,6 +4,7 @@
 // header for boolean data types true/false
 #include <stdbool.h>
 
+#define INT_MIN -9999
 
 
 
@@ -32,7 +33,7 @@ int main()
 {
     mySTACK *s1; 
     mySTACK *s2;
- 
+    int x;
     // Calling init_stack
     s1 = init_stack(10);
     //printf("\n Line 28 s1->top=%d",s1->top);
@@ -42,7 +43,9 @@ int main()
     push(s1, 1);
     push(s1, 19);
     show_stack(s1);
-
+    x = pop(s1);
+    printf("\nDeleted element is: %d\n", x);
+    show_stack(s1);
 
     return 0;
 }
@@ -71,8 +74,13 @@ mySTACK* init_stack(int max_size)
 
 void push(mySTACK *s, int x)
 {
-    s->top +=1;
-    s->array[s->top] = x;
+    if(check_stack_overflow(s))
+        printf("\t STACK overflow \n");
+    else
+    {
+        s->top +=1;
+        s->array[s->top] = x;
+    }
 }
 
 void show_stack(mySTACK *s)
@@ -82,4 +90,56 @@ void show_stack(mySTACK *s)
     for(i=0; i<=s->top; i++)
         printf("%d ", s->array[i]);
     printf("\n");
+}
+
+int pop(mySTACK *s)
+{
+    int x;
+    if(check_stack_underflow(s))
+    {
+        printf("\n STACK Underflow \n");
+        return INT_MIN;
+    }
+    else
+    {
+        x = s->array[s->top];
+        s->top -=1;
+        return x;
+    }
+}
+
+int get_top(mySTACK *s)
+{
+    int x;
+    if(check_stack_underflow(s))
+    {
+        printf("\n STACK Underflow \n");
+        return INT_MIN;
+
+    }
+    else
+    {
+        x = s->array[s->top];
+        return x;
+    }
+}
+
+bool check_stack_overflow(mySTACK *s)
+{
+    return (s->max_size == s->top);
+}
+
+bool check_stack_underflow(mySTACK *s)
+{
+    return (s->top ==-1);
+}
+
+void delete_stack(mySTACK *s)
+{
+    if(s)
+    {
+        if(s->array)
+            free(s->array);
+        free(s);
+    }
 }
